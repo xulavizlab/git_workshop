960f81f442d0d361c44a04e6c0057b7887434e2a
asaltzman <asaltzman1@gmail.com>
2019-11-27 09:19:12 -0600

    added hello world

---
+++ hello_world.py
@@ -0,0 +1,9 @@
+# In this file, you're going to introduce yourself to everyone by printing your
+# name and something about yourself.
+# Make sure to rename this file and keep the ".py".
+
+# Print your name using the print function
+print("Your name")
+
+# Add something about yourself!
+print("Hobbies? Major? Favorite color?")
